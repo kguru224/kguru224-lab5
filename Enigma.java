da9331b795ff3832ae109a6f0a4fb7dc664a3543
Kiran Guru <k.guru@gwmail.gwu.edu>
2025-09-26 18:47:41 -0400

    Implemented Encrypt Function

--- Enigma.java
+++ Enigma.java
@@ -1,85 +1,83 @@
-public class Enigma{
+public class Enigma {
 
-    private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
+    private String rotorInit[] = {
+        "#GNUAHOVBIPWCJQXDKRYELSZFMT",
         "#EJOTYCHMRWAFKPUZDINSXBGLQV",
         "#BDFHJLNPRTVXZACEGIKMOQSUWY",
         "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
-        "#TGOWHLIFMCSZYRVXQABUPEJKND"};
-
+        "#TGOWHLIFMCSZYRVXQABUPEJKND"
+    };
 
     private Rotor rotors[];
-        
-    public Enigma(int id1, int id2, int id3, String start){
 
+    public Enigma(int id1, int id2, int id3, String start) {
         rotors = new Rotor[3];
-        rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0));
-        rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));
-        rotors[2] = new Rotor(rotorInit[id3-1], start.charAt(2));
-        
+        rotors[0] = new Rotor(rotorInit[id1 - 1], start.charAt(0));
+        rotors[1] = new Rotor(rotorInit[id2 - 1], start.charAt(1));
+        rotors[2] = new Rotor(rotorInit[id3 - 1], start.charAt(2));
     }
 
+    public String decrypt(String message) {
+        String result = "";
 
-    public String decrypt(String message){        
-        //TODO
-        String result = ""; //build decrypted string
-        for (int i = 0; i < message.length(); i++) {
+        for (int i = 0; i < result.length(); i++) {
             char c = message.charAt(i);
 
-            //Find index of c in outer rotor
+            //find c on the outer rotor
             int idx = rotors[2].indexOf(c);
 
-            //Map through the middle rotor
+            //map to the middle rotor using the same index
             char map = rotors[1].charAt(idx);
 
-            //Find inex of map in the middle rotor
-            idx = rotors[1].indexOf(map);
+            //find the mapped char on the outer rotor
+            idx = rotors[2].indexOf(map);
 
-            //Map through the inner rotor
+            //map to the inner rotor and then append
             map = rotors[0].charAt(idx);
 
-            //append the result
-            result += map;
+            //add it to the result string
+            result = result + map;
 
+            // rotate it 
             rotate();
         }
+
         return result;
     }
 
-    
-    public String encrypt(String message){
-        String result = ""; //build encrypted string
-        for (int i = 0; i < message.length(); i++) {
+    public String encrypt(String message) {
+        String result = "";
+
+        for (int i = 0; i < result.length(); i++) {
             char c = message.charAt(i);
-            
-            //Find index of c in inner rotor
+
+            //find c on the inner rotor
             int idx = rotors[0].indexOf(c);
 
-            //Map through the middle rotor
-            char map = rotors[1].charAt(idx);
+            // map to the outer rotor using the same idnex
+            char map = rotors[2].charAt(idx);
 
-            //Find the index of map in the middle rotor
+            //find the mapped char on the middle rotor
             idx = rotors[1].indexOf(map);
 
-            //Map through the inner rotor
-            map = rotors[0].charAt(idx);
+            //map that index to outer rotor again
+            map = rotors[2].charAt(idx);
 
-            //append the result
-            result += map;
+            //add it to the result string
+            result = result + map;
 
-            rotate();
-
+            //rotate it 
+            rotate()
         }
 
         return result;
     }
 
-    
-    private void rotate(){
-        if(rotors[0].rotate()){
-            if(rotors[1].rotate()){
+    private void rotate() {
+        if (rotors[0].rotate()) {
+            if (rotors[1].rotate()) {
                 rotors[2].rotate();
             }
         }
     }
-    
 }
--- Rotor.java
+++ Rotor.java
@@ -1,32 +1,45 @@
 public class Rotor {
 
-    
-    private String rotorValues;
-    private char startChar;
-        
-    public Rotor(String v, char c){
-        this.rotorValues = new String(v);
+    private String rotorValues; 
+    private char startChar;     
+
+    public Rotor(String v, char c) {
+        this.rotorValues = v;
         this.startChar = c;
-        
-        while(!this.rotate());
-            
-    }
-    
-    public boolean rotate(){
 
-        char lastChar = this.rotorValues.charAt(this.rotorValues.length() - 1);
-        this.rotorValues = lastChar + this.rotorValues.substring(0, this.rotorValues.length() - 1);
-        return rotorValues.charAt(0) == this.startChar;
-               
-    }
-    
-
-    public int indexOf(char c){
-        return this.rotorValues.indexOf(c);
+        // rotate until the top equals startChar
+        while (this.rotorValues.charAt(0) != this.startChar) {
+            this.rotate();
+        }
     }
 
-    public char charAt(int idx){
+
+    //Rotate clock wise; the last character moves to the front
+    // returns true if after this rotation the top equals the starting character. 
+    public boolean rotate() {
+        // get last char
+        char last = this.rotorValues.charAt(this.rotorValues.length() - 1);
+
+        // new string: last char + everything else except last
+        this.rotorValues = last + this.rotorValues.substring(0, this.rotorValues.length() - 1);
+
+        // true if back to starting top character
+        return this.rotorValues.charAt(0) == this.startChar;
+    }
+
+
+    // Manual indexOf using only charAt
+    public int indexOf(char c) {
+        for (int i = 0; i < this.rotorValues.length(); i++) {
+            if (this.rotorValues.charAt(i) == c) {
+                return i;
+            }
+        }
+        return -1; // should not happen for valid input
+    }
+
+    // Expose character at index
+    public char charAt(int idx) {
         return this.rotorValues.charAt(idx);
     }
 }
-    
